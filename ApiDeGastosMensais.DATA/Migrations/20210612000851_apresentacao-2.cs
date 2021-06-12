f17a59a3df9dba751b7f7c2e8e4f63fd0b0e3e9f
Ted Medeiros <tedigormedeiros@gmail.com>
2021-06-11 21:51:18 -0300

    uppando migrations

---
+++ ApiDeGastosMensais.DATA/Migrations/20210612000851_apresentacao-2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiDeGastosMensais.DATA.Migrations
+{
+    public partial class apresentacao2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
